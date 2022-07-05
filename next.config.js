09a153dded748f0f4deae27e43ca15a34dd466f8
ayoubwazane <ayoubwazane306@gmail.com>
2022-07-05 22:21:22 +0100

    nextJs config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
+const withTM = require("next-transpile-modules")(["hover-effect"]);
 const nextConfig = {
   reactStrictMode: true,
-}
+  withTM,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
