4675c079dd5353101a68aed19252b8b5f51f9f55
ayoubwazane <ayoubwazane306@gmail.com>
2022-07-05 23:00:40 +0100

    adding sass package + change all css files to scss

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Hero from "../components/Hero";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.scss";
 export default function Home() {
   return (
     <div className={styles.container}>
