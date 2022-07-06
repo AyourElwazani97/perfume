e21bf07dabdbd6d296d03b4f57227b45c2efa982
ayoubwazane <ayoubwazane306@gmail.com>
2022-07-06 23:55:03 +0100

    customCursor

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.scss'
-
+import { useEffect } from "react";
+import "../styles/globals.scss";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    const CustomCursor = () => {
+      document.addEventListener("mousemove", (e) => {
+        const X = e.clientX;
+        const Y = e.clientY;
+        const cursor = document.getElementById("customCursor");
+        cursor.style.left = `${X}px`;
+        cursor.style.top = `${Y}px`;
+      });
+    };
+    CustomCursor();
+  }, []);
+  return (
+    <div>
+      <div id="customCursor"></div>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
