3d0370473521e95b0e32b9ad23fd672f55bd02a1
ayoubwazane <ayoubwazane306@gmail.com>
2022-07-09 17:36:08 +0100

    testimonial

--- components/Testimonial.js
+++ components/Testimonial.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 function Testimonial({ styles }) {
-  return <div className={styles._testimonial}>Testimonial</div>;
+  return (
+    <div id="testimonial" className={styles._testimonial}>
+      <div>
+        <h1>Testimonial</h1>
+      </div>
+      <div className={styles._testimonial_child}>
+        <div className={styles._testimonial_child_whitePapper}></div>
+      </div>
+    </div>
+  );
 }
 
 export default Testimonial;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import styles from "../styles/Home.module.scss";
 import Hero from "../components/Hero";
 import About from "../components/About";
+import Testimonial from "../components/Testimonial";
 export default function Home() {
   return (
     <div className={styles.container}>
       <Hero styles={styles} />
       <About styles={styles} />
+      <Testimonial styles={styles} />
     </div>
   );
 }
